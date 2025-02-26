8f22f086023b5ec2209a851cbf7235fa61583836
Lalit Sangore <96982204+lalitt08@users.noreply.github.com>
2025-02-26 23:32:09 +0530

    Update main.tf

--- simple/main.tf
+++ simple/main.tf
@@ -1,72 +1,73 @@
 provider "aws" {
-    region = "us-east-2"
- }
+  region = "us-east-1"
+}
 
 resource "aws_vpc" "Myvpc" {
-    cidr_block = var.vpc_cidr
-    tags = {
-      Name ="Myvpc"
-    }
+  cidr_block = var.vpc_cidr
+  tags = {
+    Name = "Myvpc"
+  }
 }
 
-resource "aws_subnet" "Publicsubnet1"{
-    vpc_id = aws_vpc.Myvpc.id
-    cidr_block = var.public_subnet_cidr
-    availability_zone = "us-east-2a"
-    tags = {
-        Name = "Publicsubnet"
-    }
+resource "aws_subnet" "Publicsubnet1" {
+  vpc_id            = aws_vpc.Myvpc.id
+  cidr_block        = var.public_subnet_cidr
+  availability_zone = "us-east-2a"
+  tags = {
+    Name = "Publicsubnet"
+  }
 }
 
-resource "aws_subnet" "Publicsubnet2"{
-    vpc_id = aws_vpc.Myvpc.id
-    cidr_block = var.public_subnet2_cidr
-    availability_zone = "us-east-2c"
-    tags = {
-        Name = "Publicsubnet2"
-    }
+resource "aws_subnet" "Publicsubnet2" {
+  vpc_id            = aws_vpc.Myvpc.id
+  cidr_block        = var.public_subnet2_cidr
+  availability_zone = "us-east-2c"
+  tags = {
+    Name = "Publicsubnet2"
+  }
 }
 
-resource "aws_subnet" "Privatesubnet1"{
-    vpc_id = aws_vpc.Myvpc.id
-    cidr_block = var.private_subnet_cidr
-    availability_zone = "us-east-2b"
-    tags = {
-        Name = "Privatesubnet1"
-    }
+resource "aws_subnet" "Privatesubnet1" {
+  vpc_id            = aws_vpc.Myvpc.id
+  cidr_block        = var.private_subnet_cidr
+  availability_zone = "us-east-2b"
+  tags = {
+    Name = "Privatesubnet1"
+  }
 }
 
-resource "aws_subnet" "Privatesubnet2"{
-    vpc_id = aws_vpc.Myvpc.id
-    cidr_block = var.private_subnet2_cidr
-    availability_zone = "us-east-2c"
-    tags = {
-        Name = "Privatesubnet2"
-    }
+resource "aws_subnet" "Privatesubnet2" {
+  vpc_id            = aws_vpc.Myvpc.id
+  cidr_block        = var.private_subnet2_cidr
+  availability_zone = "us-east-2c"
+  tags = {
+    Name = "Privatesubnet2"
+  }
 }
 
-resource "aws_internet_gateway" "igw"{
-    vpc_id = aws_vpc.Myvpc.id
-    tags = {
-        Name = "igw"
-    }
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.Myvpc.id
+  tags = {
+    Name = "igw"
+  }
 }
 
 resource "aws_route_table" "publicRT" {
-    vpc_id = aws_vpc.Myvpc.id
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.igw.id
-    }
+  vpc_id = aws_vpc.Myvpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
 }
+
 resource "aws_route_table_association" "PublicRTAssociation" {
-    subnet_id = aws_subnet.Publicsubnet1.id
-    route_table_id = aws_route_table.publicRT.id
+  subnet_id      = aws_subnet.Publicsubnet1.id
+  route_table_id = aws_route_table.publicRT.id
 }
 
 resource "aws_route_table_association" "PublicRTAssociationn" {
-    subnet_id = aws_subnet.Publicsubnet2.id
-    route_table_id = aws_route_table.publicRT.id
+  subnet_id      = aws_subnet.Publicsubnet2.id
+  route_table_id = aws_route_table.publicRT.id
 }
 
 resource "aws_nat_gateway" "Nat_gateway" {
@@ -80,8 +81,8 @@ resource "aws_nat_gateway" "Nat_gateway" {
 resource "aws_route_table" "PrivateRT" {
   vpc_id = aws_vpc.Myvpc.id
   route {
-      cidr_block = "0.0.0.0/0"
-      nat_gateway_id         = aws_nat_gateway.Nat_gateway.id
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.Nat_gateway.id
   }
   tags = {
     Name = "Private-Route-Table"
@@ -99,8 +100,8 @@ resource "aws_route_table_association" "privateRTassociationn" {
 }
 
 resource "aws_security_group" "frontendCG" {
-  vpc_id      = aws_vpc.Myvpc.id
-  name = "FrontendCG"
+  vpc_id = aws_vpc.Myvpc.id
+  name   = "FrontendCG"
   ingress {
     from_port   = 22
     to_port     = 22
@@ -155,7 +156,6 @@ resource "aws_security_group" "backendCG" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
 }
 
 resource "aws_security_group" "databaseCG" {
@@ -176,15 +176,14 @@ resource "aws_security_group" "databaseCG" {
     protocol        = "tcp"
     security_groups = [aws_security_group.backendCG.id]
   }
-
 }
 
 resource "aws_instance" "frontend_instance" {
-  ami           = var.frontendami
-  instance_type = "t2.micro"
-  key_name      = "chatappkey"
-  subnet_id     = aws_subnet.Publicsubnet1.id
-  vpc_security_group_ids = [aws_security_group.frontendCG.id]
+  ami                         = var.frontendami
+  instance_type               = "t2.micro"
+  key_name                    = "sample"
+  subnet_id                   = aws_subnet.Publicsubnet1.id
+  vpc_security_group_ids      = [aws_security_group.frontendCG.id]
   associate_public_ip_address = true
 
   tags = {
@@ -193,10 +192,10 @@ resource "aws_instance" "frontend_instance" {
 }
 
 resource "aws_instance" "backend_instance" {
-  ami           = var.backendami
-  instance_type = "t2.micro"
-  key_name      = "chatappkey"
-  subnet_id     = aws_subnet.Privatesubnet1.id
+  ami                    = var.backendami
+  instance_type          = "t2.micro"
+  key_name               = "sample"
+  subnet_id              = aws_subnet.Privatesubnet1.id
   vpc_security_group_ids = [aws_security_group.backendCG.id]
 
   tags = {
@@ -205,10 +204,10 @@ resource "aws_instance" "backend_instance" {
 }
 
 resource "aws_instance" "database_instance" {
-  ami           = var.databaseami
-  instance_type = "t2.micro"
-  key_name      = "chatappkey"
-  subnet_id     = aws_subnet.Privatesubnet2.id
+  ami                    = var.databaseami
+  instance_type          = "t2.micro"
+  key_name               = "sample"
+  subnet_id              = aws_subnet.Privatesubnet2.id
   vpc_security_group_ids = [aws_security_group.databaseCG.id]
 
   tags = {
@@ -216,168 +215,17 @@ resource "aws_instance" "database_instance" {
   }
 }
 
-resource "aws_launch_template" "backendLT" {
-  name        = "LTBackend_TF"
-  image_id    = var.backendami
-  instance_type = "t2.micro"
-  key_name    = "chatappkey"
+# Generate inventory file for Ansible
+resource "local_file" "inventory" {
+  filename = "${path.module}/inventory.ini"
+  content  = <<-EOT
+    [frontend]
+    ${aws_instance.frontend_instance.public_ip}
 
-  network_interfaces {
-    associate_public_ip_address = false
-    device_index                = 0
-    security_groups             = [aws_security_group.backendCG.id]
-  }
+    [backend]
+    ${aws_instance.backend_instance.private_ip}
 
-  tag_specifications {
-    resource_type = "instance"
-
-    tags = {
-      Name = "TFBackendLT"
-    }
-  }
+    [database]
+    ${aws_instance.database_instance.private_ip}
+  EOT
 }
-
-resource "aws_launch_template" "frontendLT" {
-  name          = "LTFrontend"
-  image_id      = var.frontendami
-  instance_type = "t2.micro"
-  key_name      = "chatappkey"
-
-  network_interfaces {
-    associate_public_ip_address = true
-    device_index                = 0
-    security_groups             = [aws_security_group.frontendCG.id]
-  }
-
-  tag_specifications {
-    resource_type = "instance"
-
-    tags = {
-      Name = "LTFrontend"
-    }
-  }
-}
-
-resource "aws_security_group" "backend_LBSG" {
-  name        = "backend-LBSG"
-  description = "Security group for backend load balancer using terraform"
-  vpc_id      = aws_vpc.Myvpc.id
-
-  ingress {
-    protocol    = "tcp"
-    from_port   = 80
-    to_port     = 80
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    protocol    = "tcp"
-    from_port   = 8000
-    to_port     = 8000
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    protocol    = "-1"
-    from_port   = 0
-    to_port     = 0
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "Backend_lb_sg"
-  }
-}
-
-resource "aws_security_group" "frontend_LBSG" {
-  name        = "frontend-LBSG"
-  description = "Security group for frontend load balancer using terraform"
-  vpc_id      = aws_vpc.Myvpc.id
-
-  ingress {
-    protocol    = "tcp"
-    from_port   = 80
-    to_port     = 80
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    protocol    = "-1"
-    from_port   = 0
-    to_port     = 0
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "Frontend_lb_sg"
-  }
-}
-
-resource "aws_lb_target_group" "backendTG" {
-  name        = "backend-tg"
-  protocol    = "HTTP"
-  port        = 8000
-  vpc_id      = aws_vpc.Myvpc.id
-  target_type = "instance"
-}
-
-
-resource "aws_lb_target_group" "frontendTG" {
-  name        = "frontend-tg"
-  protocol    = "HTTP"
-  port        = 80
-  vpc_id      = aws_vpc.Myvpc.id
-  target_type = "instance"
-}
-
-resource "aws_lb" "backendLB" {
-  name               = "lb-backend"
-  internal           = true
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.backend_LBSG.id]
-  subnets            = [aws_subnet.Privatesubnet1.id, aws_subnet.Privatesubnet2.id]
-
-  tags = {
-    Name = "lb-backend"
-  }
-}
-
-resource "aws_lb_listener" "backendlist" {
-  load_balancer_arn = aws_lb.backendLB.arn
-  port              = 80
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.backendTG.arn
-  }
-}
-
-resource "aws_lb" "frontendLB" {
-  name               = "lb-frontend-tf"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.frontend_LBSG.id]
-  subnets            = [aws_subnet.Publicsubnet1.id, aws_subnet.Publicsubnet2.id]
-
-  tags = {
-    Name = "lb-frontend"
-  }
-}
-
-resource "aws_lb_listener" "frontendlist" {
-  load_balancer_arn = aws_lb.frontendLB.arn
-  port              = 80
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.frontendTG.arn
-  }
-}
-
-
-
-
-
-    
